87e33d0c9eef3f21f7feb8c69ba2a78689266151
MaxiTrien <51158324+MaxiTrien@users.noreply.github.com>
2020-11-23 16:24:01 +0100

    composition dataframe for La defects

--- python files/df_build_descriptorsetup.py
+++ python files/df_build_descriptorsetup.py
@@ -37,7 +37,7 @@ def load_data(folder, arr):
     return df
 
 # specify abs path of folder, where you want to load the data from
-folder = r"C:\Python\Projects\crystal-phase-prediction\crystal_data\hfo2_90_80_deg"
+folder = r"C:\Python\Projects\crystal-phase-prediction\crystal_data\hfo2_La"
 arr = [f for f in os.listdir(folder) if not f.endswith('.ini')]  # change folder name here
 
 df = load_data(folder, arr)
@@ -45,5 +45,4 @@ df['name'] = arr # create column with the filenames in folder
 
 # change name to dataset that you created 
 
-pd.to_pickle(df, r"C:\Python\Projects\crystal-phase-prediction\pkl_files\structure_df_hfo2_deg.pkl")
-
+pd.to_pickle(df, r"C:\Python\Projects\crystal-phase-prediction\pkl_files\structure_df_hfo2_La_defects.pkl")
